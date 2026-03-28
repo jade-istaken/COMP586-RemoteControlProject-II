6f2422682c148b808ee3b4e875e4ea6f337920f7
jade-istaken <jade.dergevorkian@gmail.com>
2026-03-28 11:40:09 -0700

    Implemented button handling in the main program

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,37 @@ namespace RemoteControlProject
     {
         static void Main()
         {
-            Console.WriteLine(new SmartMenu());
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            ConsoleKeyInfo keyinfo;
+            Remote remote = new();
+            Screen screen = new Screen(remote);
+            IPrintable[] printables = [screen, remote];
+            bool exit = false;
+
+            do
+            {
+                if (Console.KeyAvailable)
+                {
+                    keyinfo = Console.ReadKey();
+                    switch (keyinfo.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            exit = true;
+                            break;
+                        case ConsoleKey.Enter:
+                            remote.PowerButton();
+                            break;
+                    }
+                }
+                Console.Clear();
+                foreach (var item in printables)
+                {
+                    item.PrintState();
+                }
+                Thread.Sleep(10);
+            } while(!exit);
+            Console.Clear();
         }
     }
 }
