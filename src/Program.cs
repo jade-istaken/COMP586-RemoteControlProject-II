b8bdf5e9f767578e8e397f0cf88333415664784d
jade-istaken <jade.dergevorkian@gmail.com>
2026-03-28 15:46:29 -0700

    added more button handling to main program loop

--- src/Program.cs
+++ src/Program.cs
@@ -26,6 +26,72 @@ namespace RemoteControlProject
                         case ConsoleKey.Enter:
                             remote.PowerButton();
                             break;
+                        case ConsoleKey.Multiply:
+                            remote.VolumeUp();
+                            break;
+                        case ConsoleKey.Divide:
+                            remote.VolumeDown();
+                            break;
+                        case ConsoleKey.OemPeriod:
+                            remote.MuteButton();
+                            break;
+                        case ConsoleKey.OemComma:
+                            remote.CaptionButton();
+                            break;
+                        case ConsoleKey.Add:
+                            remote.ChannelUp();
+                            break;
+                        case ConsoleKey.Subtract:
+                            remote.ChannelDown();
+                            break;
+                        case ConsoleKey.D1:
+                            remote.ButtonOne();
+                            break;
+                        case ConsoleKey.D2:
+                            remote.ButtonTwo();
+                            break;
+                        case ConsoleKey.D3:
+                            remote.ButtonThree();
+                            break;
+                        case ConsoleKey.D4:
+                            remote.ButtonFour();
+                            break;
+                        case ConsoleKey.D5:
+                            remote.ButtonFive();
+                            break;
+                        case ConsoleKey.D6:
+                            remote.ButtonSix();
+                            break;
+                        case ConsoleKey.D7:
+                            remote.ButtonSeven();
+                            break;
+                        case ConsoleKey.D8:
+                            remote.ButtonEight();
+                            break;
+                        case ConsoleKey.D9:
+                            remote.ButtonNine();
+                            break;
+                        case ConsoleKey.D0:
+                            remote.ButtonZero();
+                            break;
+                        case ConsoleKey.Home:
+                            remote.MenuButton();
+                            break;
+                        case ConsoleKey.End:
+                            remote.SettingsButton();
+                            break;
+                        case ConsoleKey.UpArrow:
+                            remote.DPadUp();
+                            break;
+                        case ConsoleKey.DownArrow:
+                            remote.DPadDown();
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            remote.DPadLeft();
+                            break;
+                        case ConsoleKey.RightArrow:
+                            remote.DPadRight();
+                            break;
                     }
                 }
                 Console.Clear();
